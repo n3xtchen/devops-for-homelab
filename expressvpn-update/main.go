4ebecf1a8d78f4e348e9cd7047314ab87a43a2de
n3xtchen <echenwen@gmail.com>
2022-09-22 17:30:29 +0800

    expressvpn-update: ansible install

--- expressvpn-update/main.go
+++ expressvpn-update/main.go
@@ -6,7 +6,7 @@ import (
     "fmt"
     "net/http"
     "os"
-    "io"
+    // "io"
     "io/ioutil"
 
     "github.com/antchfx/htmlquery"
@@ -20,42 +20,49 @@ func main() {
         os.Exit(1)
     }
 
-    fmt.Printf("Status code: %d\n", res.StatusCode)
+    // fmt.Printf("Status code: %d\n", res.StatusCode)
 
     resBody, err := ioutil.ReadAll(res.Body)
     doc, err := htmlquery.Parse(strings.NewReader(string(resBody)))
     option := htmlquery.FindOne(doc, "//option[contains(., 'Raspberry Pi OS')]")
     downloadUrl := htmlquery.SelectAttr(option, "value")
+    // signatureUrl := htmlquery.SelectAttr(option, "data-signature-uri")
 
-    fmt.Printf("download url: %s\n", downloadUrl)
+    fmt.Print(downloadUrl)
 
-    tmp := strings.Split(downloadUrl, "/")
-    fileName := tmp[len(tmp)-1]
-    fmt.Printf("File Name: %s\n", fileName)
+    // fmt.Printf("download url: %s\n", downloadUrl)
+    // fmt.Printf("signature url: %s\n", signatureUrl)
 
-    DownloadFile(downloadUrl, fileName)
+    // tmp := strings.Split(downloadUrl, "/")
+    // fileName := tmp[len(tmp)-1]
+
+    // sTmp := strings.Split(signatureUrl, "/")
+    // signatureFileName := sTmp[len(tmp)-1]
+
+    // DownloadFile(downloadUrl, fileName)
+    // DownloadFile(signatureUrl, signatureFileName)
 }
 
-func DownloadFile(url string, filepath string) error {
-    // Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
+// func DownloadFile(url string, filepath string) error {
+//     // Create the file
+// 	out, err := os.Create(filepath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer out.Close()
+// 
+// 	// Get the data
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 
+// 	// Write the body to file
+// 	_, err = io.Copy(out, resp.Body)
+// 	if err != nil {
+// 		return err
+// 	}
+// 
+// 	return nil
+// }
