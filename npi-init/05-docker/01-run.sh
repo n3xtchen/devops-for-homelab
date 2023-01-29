37e163c0064c783f29809a99a5dd05a32784c51f
n3xtchen <echenwen@gmail.com>
2023-01-29 23:59:52 +0800

    npi-init: docker 调试

--- npi-init/05-docker/01-run.sh
+++ npi-init/05-docker/01-run.sh
@@ -6,17 +6,9 @@ sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.lis
 cat files/docker-ce.gpg.key | gpg --dearmor > "${ROOTFS_DIR}/usr/share/keyrings/docker-ce.gpg"
 on_chroot << EOF
 apt-get update
-apt-get install docker-ce docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 systemctl enable docker
-systemctl start docker
-
-sudo tee /etc/docker/daemon.json <<-'EOFF'
-{
-    "registry-mirrors": ["http://docker.mirrors.ustc.edu.cn"]
-}
-EOFF
-
-systemctl daemon-reload
-systemctl restart docker
 EOF
 
+install -d                       "${ROOTFS_DIR}/etc/docker"
+install -m 644 files/daemon.json "${ROOTFS_DIR}/etc/docker/"
