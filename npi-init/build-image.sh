57bd2d34ef6baae1df2c23d100e59648ec967c49
n3xtchen <echenwen@gmail.com>
2023-02-03 15:02:48 +0800

    npi-init: 代码整理

--- npi-init/build-image.sh
+++ npi-init/build-image.sh
@@ -1,15 +1,29 @@
 cp config.tmpl pi-gen/config
 
-# add docker
+# add docker & cloud-init
 cp -r 04-docker/ pi-gen/stage2
-
-# add cloud-init
 cp -r 05-cloud-init pi-gen/stage2/
 
 cd pi-gen
 
-touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
-touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
-./build-docker.sh
+PI_ARCH="armhf"
+BRANCH=`git branch --show-current`
 
-cd -
+if [ "${BRANCH}" == "arm64" ];
+then
+    PI_ARCH="arm64"
+    sh ../replace_sources.sh
+else
+    sh ../replace_sources_armhf.sh
+fi
+
+sed -i "s/BUILD_ARCH/${PI_ARCH}/g" stage2/04-docker/files/docker.list
+
+# ./build-docker.sh
+# 
+# # 清除代码
+# git checkout -- .
+# rm -r stage2/04-docker stage2/05-cloud-init
+# rm config
+# 
+# cd -
--- npi-init/replace_sources_armhf.sh
+++ npi-init/replace_sources_armhf.sh
@@ -2,25 +2,25 @@
 
 DEBIAN_PRIMARY_SOURCE='ftp.cn.debian.org/debian'
 
-sed -i 's/x86_64|aarch64/x86_64/g' pi-gen/build-docker.sh
+sed -i 's/x86_64|aarch64/x86_64/g' build-docker.sh
 
 # 替换中文源
 REPLECE_SOURCE='RUN sed -i '"'"'s|deb.debian.org/debian|'"$DEBIAN_PRIMARY_SOURCE"'|g'"'"' /etc/apt/sources.list'
-sed -i 's#^\(RUN apt-get -y update .*\)#'"$REPLECE_SOURCE"'\n\1#g' pi-gen/Dockerfile
+sed -i 's#^\(RUN apt-get -y update .*\)#'"$REPLECE_SOURCE"'\n\1#g' Dockerfile
 
 BOOTSTRAP_URL=http://ftp.cn.debian.org/raspbian/raspbian/
-sed -i 's#\(.*bootstrap.*\) http://raspbian.raspberrypi.org/raspbian/#\1'" $BOOTSTRAP_URL"'#g' pi-gen/stage0/prerun.sh
+sed -i 's#\(.*bootstrap.*\) http://raspbian.raspberrypi.org/raspbian/#\1'" $BOOTSTRAP_URL"'#g' stage0/prerun.sh
 
 # 替换镜像中的中文源
 PI_PRIMARY_SOURCE=ftp.cn.debian.org/raspbian/raspbian
-sed -i 's#raspbian.raspberrypi.org/raspbian#'"$PI_PRIMARY_SOURCE"'#g' pi-gen/stage0/00-configure-apt/files/sources.list
+sed -i 's#raspbian.raspberrypi.org/raspbian#'"$PI_PRIMARY_SOURCE"'#g' stage0/00-configure-apt/files/sources.list
 
 PI_ARCHIVE_SOURCE='ftp.cn.debian.org/raspberrypi/debian'
-sed -i 's#archive.raspberrypi.org/debian#'"$PI_ARCHIVE_SOURCE"'#g' pi-gen/stage0/00-configure-apt/files/raspi.list
+sed -i 's#archive.raspberrypi.org/debian#'"$PI_ARCHIVE_SOURCE"'#g' stage0/00-configure-apt/files/raspi.list
 
 # 替换 DNS
 NEW_DNS='233.6.6.6'
-sed -i 's#8.8.8.8#'"$NEW_DNS"'#g' pi-gen/export-image/03-network/files/resolv.conf
+sed -i 's#8.8.8.8#'"$NEW_DNS"'#g' export-image/03-network/files/resolv.conf
 
 # fastgit 加速
-sed -i 's#github.com/\(raspberrypi/firmware\)/raw#raw.fastgit.org/\1/#g' pi-gen/export-image/05-finalise/01-run.sh
+sed -i 's#github.com/\(raspberrypi/firmware\)/raw#raw.fastgit.org/\1/#g' export-image/05-finalise/01-run.sh
