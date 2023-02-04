f9c9f359497e4f609d7574d27893759958a75b82
N3xtchen <echenwen@gmail.com>
2023-02-04 16:31:02 +0800

    npi-init: 代码整理

--- npi-init/build-image.sh
+++ npi-init/build-image.sh
@@ -9,7 +9,7 @@ cd pi-gen
 PI_ARCH="armhf"
 BRANCH=`git branch --show-current`
 
-if [ "${BRANCH}" == "arm64" ];
+if [ "${BRANCH}" = "arm64" ];
 then
     PI_ARCH="arm64"
     sh ../replace_sources.sh
@@ -19,11 +19,11 @@ fi
 
 sed -i "s/BUILD_ARCH/${PI_ARCH}/g" stage2/04-docker/files/docker.list
 
-# ./build-docker.sh
-# 
-# # 清除代码
-# git checkout -- .
-# rm -r stage2/04-docker stage2/05-cloud-init
-# rm config
-# 
-# cd -
+./build-docker.sh
+
+# 清除代码
+git checkout -- .
+rm -r stage2/04-docker stage2/05-cloud-init
+rm config
+
+cd -
